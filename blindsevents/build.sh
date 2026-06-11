e8435efc68dd51c57a8c6580771686626e4c684b
lindsay30-dev <sarah.boyom@saintjeaningenieur.org>
2026-06-11 18:58:16 +0200

    Ajout requirements.txt et mise à jour build.sh

--- blindsevents/build.sh
+++ blindsevents/build.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -o errexit
+
 pip install --upgrade pip
 pip install -r requirements.txt
+
+# Se déplacer dans le dossier contenant manage.py
+cd blindsevents
+
 python manage.py collectstatic --noinput
 python manage.py migrate
